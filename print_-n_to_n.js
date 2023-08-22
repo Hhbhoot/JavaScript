1613c146f025395297ea8f0bbc5abfa941baf7cd
Hitesh <hiteshbhut666@gmail.com>
2023-08-22 16:18:12 +0530

    loops

---
+++ print_-n_to_n.js
@@ -0,0 +1,9 @@
+let a= -50 ;
+let txt = "";
+
+a=50;
+do{
+    txt += a + " ";  
+}while(a<=50);
+
+console.log(txt);
---
+++ print_1_to_n.js
@@ -0,0 +1,10 @@
+let a;
+let txt = "";
+
+a=1;
+while(a<100)
+{
+    txt += a + " ";
+    a++;
+}
+console.log(txt);
