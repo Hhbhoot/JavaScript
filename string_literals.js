a8fc03701d24ba9eeb4c0e640d6fe17e68e0f165
Hitesh <hiteshbhut666@gmail.com>
2023-08-23 18:03:51 +0530

    string_literal

---
+++ string_literals.js
@@ -0,0 +1,11 @@
+// string literals => ``==> backtick ==> show String as it enterd 
+
+let hello = "Hello ";
+let Hello = "Skill Qode"
+
+console.log(`this is string : ${hello } ${Hello}`)
+
+let a = `hello world
+i am Hitesh`
+         
+ console.log(a);
