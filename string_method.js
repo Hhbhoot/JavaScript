1b82ea288bc485ec9be930f2244a3c06ab65bbb6
Hitesh <hiteshbhut666@gmail.com>
2023-08-24 19:28:46 +0530

    String_Methods

---
+++ string_method.js
@@ -0,0 +1,46 @@
+// string length
+
+var a = "Hello world i am Hitesh";
+
+var length = a.length;
+console.log(length);
+
+//toUpperCase => converts string into upper case
+
+// var text = a.toUpperCase();
+
+// console.log(text);
+
+// toLowerCase => converts string into lower case
+
+var text = a.toLowerCase();
+console.log(text);
+
+var b = "I am From surat"
+
+var text = a.concat(" \n",b);
+console.log(text);
+
+var c = a.slice(4,9)
+console.log(c);
+
+var c = a.substring(5 , 10)
+console.log(c);
+
+var c = a.substring(-5);
+console.log(c);
+
+var c = a.substr(-10 , 15 );
+console.log(c);
+
+var c = a.slice(-8 , -1);
+console.log(c);
+
+var c = a.substr(-7,6);
+console.log(c);
+
+
+
+
+
+
