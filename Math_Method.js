7ed1612a9e530e495ca5d54a3b97df198afe2cf5
Hhbhoot <hiteshbhoot938@gmail.com>
2023-08-26 20:38:43 +0530

    Math Method & Properties

---
+++ Math_Method.js
@@ -0,0 +1,39 @@
+console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.ceil(4.5));   // 5
+console.log(Math.ceil(-4.5));  // -4
+console.log(Math.floor(4.3)) // 4
+console.log(Math.floor(-4.3)) // -5
+console.log(Math.round(4.3)) // 4
+console.log(Math.round(-4.3)) // 4
+console.log(Math.abs(-4.3)) // 4
+console.log(Math.abs(50.2)) // 50 .2 
+console.log(Math.pow(2,3)); // 8
+console.log(Math.pow(0,10)) // 0 
+console.log(Math.pow(10,0));  // 1
+// 
+console.log(Math.LN2)
+console.log(Math.LN10)
+console.log(Math.log10(100))
+console.log(Math.LOG10E)
+console.log(Math.LOG2E)
+console.log(Math.SQRT2)
+console.log(Math.SQRT1_2)
+console.log(Math.sin(30 * Math.PI / 180))
+console.log(Math.cos(30 * Math.PI / 180))
+console.log(Math.tan(30 * Math.PI / 180))
+console.log(Math.cosh(30 * Math.PI  / 180))
+console.log(Math.tanh(30 * Math.PI  / 180))
+console.log(Math.sinh(30 * Math.PI  / 180))
+console.log(Math.acosh(9))
+console.log(Math.trunc(4.5)) // 4
+console.log(Math.trunc(44.5)) // 44
+
+console.log(Math.sign(44)); // 1
+console.log(Math.sign(-44)); // -1
+console.log(Math.sign(0)); // 0
+
+console.log(Math.min(1,2,33,-1)) // -1
+console.log(Math.min(1,2,33)) 
+console.log(Math.max(1,2,33)) 
+
