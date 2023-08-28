e8e7c5c347fbccdf114256d16f04027014c84ada
Hitesh <hiteshbhut666@gmail.com>
2023-08-28 17:05:52 +0530

    Number Methods

---
+++ Number_MEthods.js
@@ -0,0 +1,10 @@
+var x =5 ;
+console.log(x.toString());
+
+ var x = 9656514849844 ;
+
+ console.log(x.toExponential(2));
+ console.log(x.toExponential(5));
+
+ var x = 124.2354;
+ console.log(x.toFixed(3))
