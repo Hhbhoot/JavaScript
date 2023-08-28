95b6de7edb13a231c0b25e5b87b39b003f32866a
Hitesh <hiteshbhut666@gmail.com>
2023-08-28 17:15:34 +0530

    Number Methods

--- Number_MEthods.js
+++ Number_MEthods.js
@@ -8,3 +8,12 @@ console.log(x.toString());
 
  var x = 124.2354;
  console.log(x.toFixed(3))
+
+ console.log(x.toPrecision(4))
+ console.log(x.toPrecision(5))
+ console.log(x.valueOf());
+
+ console.log(Number.isInteger(x));
+
+ x= 2 ;
+ console.log(Number.isInteger(x));
