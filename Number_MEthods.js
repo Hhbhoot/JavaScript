650469487065a340382538392daeb46ec9d85b36
Hhbhoot <hiteshbhoot938@gmail.com>
2023-08-29 12:18:30 +0530

    Number Methods

--- Number_MEthods.js
+++ Number_MEthods.js
@@ -17,3 +17,78 @@ console.log(x.toString());
 
  x= 2 ;
  console.log(Number.isInteger(x));
+
+ var c ="12354";
+
+
+ console.log(Number(c));
+
+ var c ="2546";
+ console.log(Number(c));
+
+ var c = false;
+
+ console.log(Number(c));
+
+ var c = 2.6488;
+
+console.log(parseFloat(c));
+console.log(parseInt(c));
+
+var  c = "-10";
+console.log(parseInt(c));
+
+var  c = "-10 10 20";
+console.log(parseInt(c));
+
+var  c = "10 years";
+console.log(parseInt(c));
+
+var  c = " years 10";
+console.log(parseInt(c));
+
+var d = "0.2335 22" ;
+
+console.log(parseFloat(d));
+
+var d = "1.022 22" ;
+console.log(parseFloat(d));
+
+var d = "1.5 years" ;
+console.log(parseFloat(d));
+
+var d = "years 10.5" ;
+console.log(parseFloat(d));
+
+var e = 10.5 ;
+console.log(Number.isSafeInteger(e));
+
+var e = 100 ;
+console.log(Number.isSafeInteger(e));
+
+var e = 100000000000000000 ; //==> safe integer Range  -(2**53 - 1) to +(2**53 - 1).
+console.log(Number.isSafeInteger(e));
+
+var e = 100000000000000000 ; //==> safe integer Range  -(2**53 - 1) to +(2**53 - 1).
+console.log(e.toExponential(0));
+
+var f = "100";
+console.log(Number.isSafeInteger(f)); // false
+
+var f = 100;
+console.log(Number.isSafeInteger(f)); // true
+
+var f = 10000.1;
+console.log(Number.isInteger(f)); // false
+
+var f = 10000;
+console.log(Number.isInteger(f)); // false
+
+var g = " 1.5 Hitesh"
+console.log(Number.parseFloat(g));
+
+var g = " 1.25478"
+console.log(Number.parseFloat(g));
+
+var g = " 1.5"
+console.log(Number.parseInt(g));
