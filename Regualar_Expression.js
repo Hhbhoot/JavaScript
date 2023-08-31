52fcb856e7ad8a3a3544f2edaa2c3c9a3234be4a
Hitesh <hiteshbhut666@gmail.com>
2023-08-31 17:12:27 +0530

    Regular Expression

---
+++ Regualar_Expression.js
@@ -0,0 +1,14 @@
+// let data = "india is my country . i am from india"
+// let reg = /india/g ;
+// data =data.replace(reg,"sri lanka");
+
+// console.log(data);
+
+// let reg = /i/g;
+// data = data.replace(reg,"ii")
+//  console.log(data);
+
+let data = "india is my country . i am from india,122341"
+let reg = /\d/g;
+
+
