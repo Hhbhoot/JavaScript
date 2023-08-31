4089748fbf6a0f17a7f7bf9692b8e0f7fb0dc876
Hhbhoot <hiteshbhoot938@gmail.com>
2023-08-31 15:12:58 +0530

    Date Methods

---
+++ Date_Methods.js
@@ -0,0 +1,98 @@
+// // var time = new Date();
+
+// // console.log(time);
+// // var time = new Date(2020,9,12);
+// // console.log(time);
+
+// // console.log(time.getMonth())
+// // console.log(time.getFullYear())
+// // console.log(time.getDay())
+// // console.log(time.getDate())
+
+// // var time = new Date();
+
+// // console.log(time);
+// // var time = new Date("2020-8-11 ");
+// // console.log(time);
+
+// // var time = new Date(2020 ,2 );
+// // console.log(time);
+
+// // var time = new Date(2010 ,2 ,5 );
+// // console.log(time);
+
+// // var time = new Date(2010 ,2 ,5 , 4 );
+// // console.log(time);
+
+// // var time = new Date(2010 ,2 ,5 , 4 ,30);
+// // console.log(time);
+
+// // var time = new Date(2010 ,2 ,5 , 4 ,30 , 45);
+// // console.log(time);
+
+// // var time = new Date(2010 ,2 ,5 , 4 ,30 , 45,78);
+// // console.log(time);
+
+// // var time = new Date(2010 ,2 ,5 , 4 ,30 , 45,78);
+// // time.toString();
+// // console.log(time);
+
+// /* *********** GET METHODS ********** */
+
+// // var time = new Date();
+// // console.log(time);
+
+// // console.log(time.getDate());
+// // console.log(time.getDay());
+// // console.log(time.getFullYear());
+// // console.log(time.getMonth());
+// // console.log(time.getHours());
+// // console.log(time.getMinutes());
+// // console.log(time.getSeconds());
+// // console.log(time.getMilliseconds());
+// // console.log(time.getTime());  // Count millisecond since 1 january 1970
+
+
+// /************* SET METHODS *********/
+
+// //  var d = new Date();
+// //  d.setMonth(2);
+// //  d.setDate(10);
+// //  d.setFullYear(2000);
+
+// //  console.log(d);
+
+// // var d = new Date();
+// // d.setDate(20);
+// // console.log(d);
+
+// // // var d = new Date();
+// // // d.setDay(6);
+// // // console.log(d);
+
+// // var d = new Date();
+// // d.setFullYear(2002);
+// // console.log(d);
+
+// // var d = new Date();
+// // d.setHours(20);
+// // console.log(d);
+
+// // var d = new Date();
+// // d.setMinutes(20);
+// // console.log(d);
+
+// // var d = new Date();
+// // d.setSeconds(20);
+
+// // var d = new Date();
+
+// // d.setMilliSeconds(232)
+// // console.log(d);
+
+// var d = new Date();
+// d.setTime();
+// console.log(d);
+
+
+
