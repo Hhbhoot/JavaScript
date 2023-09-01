8b683b710022755b32406b2213ea28bbdf4f582c
Hitesh <hiteshbhut666@gmail.com>
2023-09-01 16:57:18 +0530

    Basic Function

---
+++ Basic_function.js
@@ -0,0 +1,49 @@
+// No argument no Return
+
+// function Hello()
+// {
+//     console.log("Hello World...");
+// }
+// Hello();
+
+// No Argument With Return
+
+// function product()
+// {
+//      let a = 5 , b = 10; 
+//      return a  * b ;
+// }
+
+// product();
+// console.log(product());
+
+// With Argument With Return
+
+// function square(a)
+// {
+//     return a * a ;
+
+// }
+// let x =square(10);
+// console.log(x);
+
+// With Argument No Return
+
+// function cube(a)
+// {
+//     console.log(a *a * a);
+// }
+
+// cube(100);
+
+
+
+// Anonymos Function
+// Function as Expression
+// Function as a variable
+
+let add = function(a,b)
+{
+    return a +b ;
+}
+console.log(add(10,4));
