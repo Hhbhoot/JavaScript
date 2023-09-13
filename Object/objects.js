822202d9234cbea9b376e09983dd6549b4e8466c
Hitesh <hiteshbhut666@gmail.com>
2023-09-13 17:03:10 +0530

    Objects

---
+++ Object/objects.js
@@ -0,0 +1,17 @@
+
+let person = {
+     name : "Hitesh" ,
+     Age : 21 ,
+     No : 54655445,
+     Fullname : function(){
+
+        return this.name  + " " + this.Age ;
+     }
+
+};
+console.log(person.name);
+// console.log(person.Age);
+// console.log(person.No);
+// console.log(person.Fullname);
+
+// console.log(person);
