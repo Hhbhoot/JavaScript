0b14e12f835bc34cf926b1267a5f134bfa5e8424
Hhbhoot <hiteshbhoot938@gmail.com>
2023-09-21 11:20:18 +0530

    Dom Method

---
+++ Function/setTimeOut()_Function.js
@@ -0,0 +1,66 @@
+// function greet(){
+//      console.log("Hello World..");
+// }
+
+// var x = setTimeout(greet,5000);
+
+// function time(){
+
+// var newTime = new Date();
+
+// var x = newTime.toLocaleTimeString();
+
+// console.log(x);
+
+// // let id = setTimeout(time,2000);
+// clearTimeout(id);                 // => To stop the settimeout()
+
+// }
+
+// function greeting() {
+//   console.log("Hello , Good Morning..");
+// }
+
+// let intervalId = setTimeout(greeting, 1000);
+
+// console.log("ID :" + intervalId);
+
+// ClearrTimeOut()
+
+// let counting = 0;
+
+// function count() {
+//   counting += 1;
+
+//   console.log(counting);
+// }
+
+// let id = setTimeout(count, 1000);
+// clearTimeout(id);
+
+// passing Argument in settimeout()
+
+// function Hitesh(arg1,arg2)
+// {
+    // console.log("Hello " + arg1 + arg2) ;
+// }
+
+// let x = setTimeout(Hitesh , 2000,"world ",'I Am Hitesh Bhoot')
+
+// progam that shows delay in execution
+function greet1(){
+    console.log("Hello World Good Morning");
+}
+function hello(name){
+
+    console.log("Hello" + ' ' + name);
+}
+
+ setTimeout(greet1 , 3000);
+hello('Hitesh Bhoot');
+
+
+
+
+
+
